3a8a2957d69a62c143127aef4132553d037909c0
JonathanLima25 <jonathan.lima.sjc23@gmail.com>
2020-03-12 18:34:03 -0300

    adicionando bootstrap com template

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config['TITLE'] = "Padaria"
 Bootstrap(app)
 
 @app.route('/')
 def index():
     products = ['Ciabatta', 'Baguete', 'Pretzel']
-    return products[0]
+    return render_template('index.html', products=products)
 
